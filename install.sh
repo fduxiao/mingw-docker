e95877ec8bb2fc7f27c6758dc99043136e7afb71
fduxiao <JerryTansWork@hotmail.com>
2018-09-20 03:27:14 +0800

    alert if a package fails to install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo -u pkg git clone https://aur.archlinux.org/$1.git
-cd $1 && sudo -u pkg makepkg
-pacman -U --noconfirm $1-*.tar.xz
+sudo -u pkg git clone https://aur.archlinux.org/$1.git && \
+cd $1 && sudo -u pkg makepkg && \
+pacman -U --noconfirm $1-*.tar.xz && \
 cd .. && rm -rf $1
 
